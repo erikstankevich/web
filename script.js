12bcc6ef3c094edbe85d4c2487e7c35a62da9c9e
ees <21estankevich@gmail.com>
2025-11-26 19:22:38 +0100

    converts

--- script.js
+++ script.js
@@ -11,4 +11,38 @@ button.addEventListener("click", () => {
 });
 
 
+fileInput.addEventListener("change", () => {
+	const file = fileInput.files[0];
+	const reader = new FileReader();
+	reader.onload = function(e) {
+		const content = e.target.result;
 
+		const newBlob = new Blob([content], { type: file.type });
+
+		const a = document.createElement("a");
+		a.href = URL.createObjectURL(newBlob);
+
+		let newName = file.name;
+		if (file.name.toLowerCase().endsWith(".lrv")){
+			newName = file.name.slice(0, -4) + ".mp4";
+		} else {
+			return;
+		}
+
+
+
+
+
+
+		a.download = newName;
+
+		a.click();
+
+		URL.revokeObjectURL(a.href);
+	};
+
+	reader.readAsArrayBuffer(file);
+});
+
+
+;
