3e7a251d4c095b9830837dc9e9e7447819a48a92
Erik Stankevich | MikroPlan GmbH <ees@mikroplan.com>
2025-12-02 09:12:21 +0100

    spacing problem

--- anadolu/script.js
+++ anadolu/script.js
@@ -1,6 +1,12 @@
-window.addEventListener("load", () => {
+window.addEventListener("DOMContentLoaded", () => {
   const community = document.getElementById("community");
   const rightside = document.querySelector(".right-side");
+
+  if (!community || rightside)
+  {
+    console.error("null");
+    return;
+  }
 
   const communityHeight = community.offsetHeight;
   rightside.style.paddingTop = communityHeight + "px";
