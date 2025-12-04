1094313e8569d3a8b2f43797211c0842b060d2f4
Erik Stankevich | MikroPlan GmbH <ees@mikroplan.com>
2025-12-04 19:12:22 +0100

    auto

--- slots/script.js
+++ slots/script.js
@@ -5,25 +5,30 @@ const reel2 = document.getElementById("reel2");
 const reel3 = document.getElementById("reel3");
 
 const spin= document.getElementById("spin");
+const auto=document.getElementById("auto");
 const result = document.getElementById("result");
 const money = document.getElementById("money");
 
 let moneyvalue = 10;
 money.textContent = "$ " + moneyvalue;
 
+let autoInterval = null;
+
 function randomEmoji() {
   return emojis[Math.floor(Math.random() * emojis.length)];
 }
 
 
-spin.addEventListener("click", () => {
 
+function doSpin() 
+{
   if (moneyvalue <= 0) {
     result.textContent = "0"
     return;
   }
   
-  moneyvalue -= 1;
+  moneyvalue -= 0.1;
+  money.textContent = "$ " + Math.floor(moneyvalue*10)/10;
 
 
   const r1 = randomEmoji();
@@ -33,6 +38,30 @@ spin.addEventListener("click", () => {
   reel1.textContent = r1;
   reel2.textContent = r2;
   reel3.textContent = r3;
+}
+
+spin.addEventListener("click", doSpin);
+
+
+  auto.addEventListener("click", () => {
+    if (autoInterval) {
+      clearInterval(autoInterval);
+      autoInterval = null;
+      auto.textContent = "auto";
+      return;
+    }
+
+    auto.textContent = "stop";
+
+    autoInterval = setInterval(() => {
+      if (moneyvalue <= 0) {
+        clearInterval(autoInterval);
+        autoInterval = null;
+        auto.textContent = "auto";
+        return;
+      }
+      doSpin();
+    }, 1000);
 });
 
 
