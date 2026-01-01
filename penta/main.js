6466889617ac316f3029bf47e8b131fcabd9379c
ees <21estankevich@gmail.com>
2026-01-01 21:10:34 +0100

    js done

---
+++ penta/main.js
@@ -0,0 +1,30 @@
+const icon_width = 79,
+      icon_height = 79,
+      num_icons = 9,
+      time_per_icon = 100;
+
+let indexes = [0, 0, 0, 0, 0];
+
+const roll = (reel, offset = 0) => {
+  const delta  = (offset +2) * num_icons + Math.floor(Math.random() * num_icons); 
+
+  const style = getComputedStyle(reel);
+  const backgroundPositionY = parseFloat(style.backgroundPositionY) || 0;
+
+  reel.style.transition = `background-position-y ${delta * time_per_icon}ms ease-out`;
+  reel.style.backgroundPositionY = `${backgroundPositionY + delta * icon_height}px`;
+}
+
+
+const rollAll = () => {
+  const reelList = document.querySelectorAll('.slots > .reel');
+  reelList.forEach((reel, i) => {
+    roll(reel, 1);
+  });
+}
+
+
+
+
+rollAll();
+
